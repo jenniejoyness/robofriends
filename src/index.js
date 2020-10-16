1280f29b138ccd402635f1574ec672b670071c67
jennifer klein <jenniejoyness@gmail.com>
2020-10-16 11:54:26 +0300

    card and card list components

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CardList from './CardList'
+import 'tachyons';
+
+
+ReactDOM.render(
+  <div>
+    <CardList />
+  </div>,
+  document.getElementById('root')
+);
+
